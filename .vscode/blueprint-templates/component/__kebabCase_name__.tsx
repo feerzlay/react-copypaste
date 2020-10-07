c7920388f8772af81d7c506d6bec025241979847
Denis Yakshov <feerzlay@gmail.com>
2020-10-07 13:30:13 +0300

    feat: add blueprint template for react component

---
+++ .vscode/blueprint-templates/component/__kebabCase_name__.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { I{{pascalCase name}}Props } from './{{kebabCase name}}.types';
+
+export const {{pascalCase name}}: React.FC<I{{pascalCase name}}Props> = () => {
+  return null;
+};
